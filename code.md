f945031836abb48482c1c6f7050a9d20f290ff47
haizhuangbu <buhaizhuang123@163.com>
2024-03-27 21:51:35 +0800

    配置登录基本信息

---
+++ code.md
@@ -0,0 +1,18 @@
+1. vue init webpack project_name 初始化项目
+
+2. 使用webstorm 打开
+
+3. npm run dev 启动前端项目
+
+4. 导入 element-ui 框架
+
+5. 设置 element-ui 全局配置 编辑 main.js 文件
+
+   5.1
+
+   ```
+   import ElementUI from "element-ui"; // ui框架导入
+   import 'element-ui/lib/theme-chalk/index.css' // ui css 样式导入
+   ```
+目录结构：
+  login: 登录页面
---
+++ src/view/login/login.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="body">
+    <el-container>
+
+      <el-header id="body_head">
+        <h1 style="color: #2c3e50">小布庄园</h1>
+      </el-header>
+      <el-main>
+
+        <el-form id="login_form" label-width="100px" :model="loginUser">
+          <el-form-item label="用户名:">
+            <el-input type="text" name="username" v-model="loginUser.username"></el-input>
+          </el-form-item>
+
+          <el-form-item label="密码:">
+            <el-input type="text" name="password" v-model="loginUser.password"></el-input>
+          </el-form-item>
+
+          <el-form-item label="验证码:">
+            <el-input type="text" name="qrCde" v-model="loginUser.qrCde"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-row type="flex" justify="center">
+              <el-col :span="4">
+                <el-button type="primary" style="width: 100px">登录</el-button>
+              </el-col>
+            </el-row>
+
+          </el-form-item>
+        </el-form>
+
+
+      </el-main>
+      <el-footer></el-footer>
+    </el-container>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  // 登录页面
+  name: "login",
+  data() {
+    return {
+      loginUser: {
+        username: '',
+        password: '',
+        qrCde: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+#body {
+  background-image: url("../../../static/image/bb9677aa0d90a0fc621d097529f8daf8.jpg");
+  width: 100%;
+}
+#login_form .el-input {
+  width: 25rem;
+}
+
+#login_form {
+  padding-top: 25rem;
+  font-weight: bold;
+  font-family: 微软雅黑;
+  float: right;
+}
+
+
+
+
+</style>
